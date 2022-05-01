972eaf3aa2e71df8e7d8cbea3a8740f8d7ffd082
CoderTeam2 <72189661+CoderTeam2@users.noreply.github.com>
2022-05-01 09:30:26 +0530

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-function addUsers(){
-    localStorage.setItem("Player1", document.getElementById("player1").value);
-    localStorage.setItem("Player2", document.getElementById("player2").value);
-    window.location = "index2.html";
-}
